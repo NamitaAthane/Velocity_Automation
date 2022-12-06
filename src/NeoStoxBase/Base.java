81ce5b332447add091297b56c7cd08bfbde7c61a
Namita <namitaathane@gmail.com>
2022-12-06 18:00:58 +0530

    added space in Base class

--- src/NeoStoxBase/Base.java
+++ src/NeoStoxBase/Base.java
@@ -9,10 +9,13 @@ public class Base {
 	
 	protected static WebDriver driver=null;
 	
+	
 	public void launchBrowser() {
 		
 		System.setProperty("webdriver.chrome.driver","E:\\Nam_Software\\chromedriver.exe");
-		 driver=new ChromeDriver();
+		 
+		driver=new ChromeDriver();
+		
 		driver.get("https://neostox.com/sign-in");
 		NeoUtility.ImplicitWaitCall(driver, 500);
 		
